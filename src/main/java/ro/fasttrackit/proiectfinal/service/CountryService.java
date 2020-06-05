dd1faf74c9188c18a41bb9126ad3c0c2d89dc243
Ciurdar <ciurdar_stefania@yahoo.com>
2020-06-05 14:19:48 +0300

    proiect-final

---
+++ src/main/java/ro/fasttrackit/proiectfinal/service/CountryService.java
@@ -0,0 +1,46 @@
+package ro.fasttrackit.proiectfinal.service;
+
+import org.springframework.stereotype.Service;
+import ro.fasttrackit.proiectfinal.country.Country;
+import ro.fasttrackit.proiectfinal.repository.CountryRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CountryService {
+    private final CountryRepository repository;
+
+    public CountryService(CountryRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Country> getAll() {
+        return repository.findAll();
+    }
+
+    public Country getCountryOrThrow(final Integer id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Could not find movie with id " + id));
+    }
+
+    public Optional<Country> getById(final Integer id) {
+        return repository.findById(id);
+    }
+
+    public Country addCountry(Country country) {
+        return repository.save(country);
+    }
+
+    public Country replaceCountry(Country country, Integer id) {
+        country.setId(id);
+        return repository.save(country);
+    }
+
+    public Country deleteCountry(Integer id) {
+        Country country = getCountryOrThrow(id);
+        repository.deleteById(id);
+        return country;
+    }
+
+}
